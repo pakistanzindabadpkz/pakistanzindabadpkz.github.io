b5981d0cc6bf676eb55fcfb89a6207d764994bdf
khan Arbaz Ali <infopakistanzindabadpkz@gmail.com>
2026-08-11 19:30:08 +0500

    Improve copyContract function and alerts
    Refactor copyContract function to trim whitespace and update alert messages.

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 function copyContract() {
-  const text = document.getElementById("contractAddress").innerText;
 
-  navigator.clipboard.writeText(text).then(() => {
-    alert("Contract address copied!");
-  }).catch(err => {
-    alert("Failed to copy address: " + err);
-  });
+    const contract = document.getElementById("contractAddress").innerText.trim();
+
+    navigator.clipboard.writeText(contract)
+        .then(() => {
+            alert("PKZ contract address copied!");
+        })
+        .catch(() => {
+            alert("Unable to copy contract address.");
+        });
+}
+
+
+function toggleMenu() {
+
+    const menu = document.querySelector(".nav-links");
+
+    if (menu.style.display === "flex") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+        menu.style.flexDirection = "column";
+        menu.style.position = "absolute";
+        menu.style.top = "80px";
+        menu.style.right = "5%";
+        menu.style.background = "#003d1d";
+        menu.style.padding = "25px";
+        menu.style.gap = "20px";
+    }
+
 }
